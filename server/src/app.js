071707f49d070f28874b1dc3fb6d8f97491f2dd2
Pradyumna Chipde <pradyumnachipde41@gmail.com>
2026-06-04 20:18:40 +0530

    Authentication module structure done

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Smart Travel Companion API"
-    });
+  res.json({
+    success: true,
+    message: "Smart Travel Companion API",
+  });
 });
+
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
---
+++ server/src/controllers/authController.js
@@ -0,0 +1,13 @@
+exports.registerUser = async (req, res) => {
+  res.json({
+    success: true,
+    message: "Register route working",
+  });
+};
+
+exports.loginUser = async (req, res) => {
+  res.json({
+    success: true,
+    message: "Login route working",
+  });
+};
